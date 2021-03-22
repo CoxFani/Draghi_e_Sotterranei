740a86c1f607e2dfaabb1bd0310c2416a0b1a491
ippo1997 <gabriele.ippoliti@stud.unifi.it>
2021-03-22 16:32:16 +0100

    escludo test dado main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "Dice.h"
 
 int main() {
-    int value=0;
+    /* int value=0;
     Dice d20(20);
-    for(int i= 0; i<50; i++) {          /* <-PROVA DADO */
+    for(int i= 0; i<50; i++) {          <-PROVA DADO
         value = d20.roll(1);
         std::cout<< value << std::endl;
-    }
+    } */
     return 0;
 }
