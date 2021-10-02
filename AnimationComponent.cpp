f7271e3bdc61c3ffece8e8b65d4e0f5183a8e971
ippo1997 <gabriele.ippoliti@stud.unifi.it>
2021-10-02 19:42:44 +0200

    Classe Animation

---
+++ AnimationComponent.cpp
@@ -0,0 +1,13 @@
+//
+// Created by gabriele on 02/10/21.
+//
+
+#include "AnimationComponent.h"
+
+AnimationComponent::AnimationComponent() {
+
+}
+
+AnimationComponent::~AnimationComponent() {
+
+}
