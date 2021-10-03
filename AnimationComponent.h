c257167b1b5f4e9f44bdb88d96325654d1383e61
ippo1997 <gabriele.ippoliti@stud.unifi.it>
2021-10-03 10:16:40 +0200

    AnimationComponent continuo implementazione

--- AnimationComponent.h
+++ AnimationComponent.h
@@ -17,36 +17,59 @@
 class AnimationComponent {
 
 public:
-    AnimationComponent();
+    AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet);
     virtual ~AnimationComponent();
+
+    void addAnimation(const std::string key);
+    void startAnimation(const std::string animation);
+    void pauseAnimation(const std::string animation);
+    void resetAnimation(const std::string animation);
+    void update(const float& dt);
 
 private:
     class Animation {
     public:
+        sf::Sprite& sprite;
         sf::Texture& textureSheet;
-        float speed;
+        float animationTimer;
+        float timer;
         int width;
         int height;
         sf::IntRect startRect;
+        sf::IntRect currentRect;
         sf::IntRect endRect;
-
-        Animation(sf::Texture& textureSheet, float speed, int start_x, int start_y, int end_x, int end_y, int width, int height)
-                : textureSheet(textureSheet), speed(speed), width(width), height(height)
+//Animation non inizializza timer (fare caso quando apre class Animation{} se è una dimenticanza o se fa così)
+        Animation(sf::Sprite& sprite, sf::Texture& texture_sheet, float animation_timer, int start_x, int start_y, int end_x, int end_y, int width, int height)
+                : sprite(sprite), textureSheet(texture_sheet), animationTimer(animation_timer), width(width), height(height)
         {
-            this->width = width;
-            this->height = height;
             this->startRect = sf::IntRect(start_x, start_y, width, height);
+            this->currentRect = this->startRect;
             this->endRect = sf::IntRect(end_x, end_y, width, height);
+
+            this->sprite.setTexture(this->textureSheet, true);
+            this->sprite.setTextureRect(this->startRect);
         }
 
         void update(const float& dt){
+            this->timer = 10.f * dt;
+            if (this->timer >= this->animationTimer){
+                this->timer = 0.f;
 
+                if (this->currentRect != this->endRect) {
+                    this->currentRect.left += this->width;
+                }
+                else {
+                    this->currentRect.left = this->startRect.left;
+                }
+            }
         }
 
         void pause();
         void reset();
     };
 
+    sf::Sprite& sprite;
+    sf::Texture& textureSheet;
     std::map<std::string, Animation> animations;
 };
 
