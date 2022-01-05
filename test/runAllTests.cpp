a69f49e432ede45489bdf57d429968ee7043ab71
CoxFani <cosimo.fani@stud.unifi.it>
2022-01-05 13:15:57 +0100

    prova HeroTest

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -1,6 +1,25 @@
 #include "gtest/gtest.h"
+#include "precompiler.h"
+#include "../Hero.h"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+struct HeroTest : testing::Test
+{
+    Hero* hero;
+
+    sf::Texture& texture_sheet;
+
+
+    HeroTest()
+    {
+        hero = new Hero(200, 200, texture_sheet);
+    }
+    virtual ~HeroTest()
+    {
+        delete hero;
+    }
+};
