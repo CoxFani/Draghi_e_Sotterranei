ea5753bdea881a24e3cb473bb2b0b3916ca1393a
CoxFani <cosimo.fani@stud.unifi.it>
2022-01-07 17:07:01 +0100

    pulizia

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -2,28 +2,13 @@
 #include "precompiler.h"
 #include "../Hero.h"
 #include "../Enemy.h"
-#include "../EnemySpawnerTile.h"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
-/*
-struct HeroTest : testing::Test
-{
-    Hero* hero;
 
-    HeroTest()
-    {
-        hero = new Hero(200, 200);
-    }
-    virtual ~HeroTest()
-    {
-        delete hero;
-    }
-};
- */
 
 struct EnemyTest : testing::Test
 {
@@ -31,7 +16,7 @@ struct EnemyTest : testing::Test
 
     EnemyTest()
     {
-        auto enemySpawnerTile = new EnemySpawnerTile(400, 400, 48, 1, 1, 60, 300);
+        auto enemySpawnerTile = new EnemySpawnerTile(300, 300, 48, 1, 1, 60, 1000);
         auto hero = new Hero(200, 200);
         enemy = new Enemy(300, 300, reinterpret_cast<EnemySpawnerTile &>(enemySpawnerTile),
                           reinterpret_cast<GameCharacter &>(hero));
@@ -49,18 +34,3 @@ TEST_F(EnemyTest, EnemyAttacksHero){
     EXPECT_LT(vectorDistance(enemy->getPosition(), hero->getPosition()), 200.f);
 }
 
-/*
-struct EnemySpawnerTileTest : testing::Test
-{
-    EnemySpawnerTile* enemySpawnerTile;
-
-    EnemySpawnerTileTest()
-    {
-        enemySpawnerTile = new EnemySpawnerTile(400, 400, 48, 1, 1, 60, 300);
-    }
-    virtual ~EnemySpawnerTileTest()
-    {
-        delete enemySpawnerTile;
-    }
-};
- */
