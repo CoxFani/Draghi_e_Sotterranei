80e7125206208e08dadb3572f09c1f5e0010622b
ippo1997 <gabriele.ippoliti@stud.unifi.it>
2022-01-09 11:03:45 +0100

    EnemyTest no puntatori

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -8,29 +8,21 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-
-
 struct EnemyTest : testing::Test
 {
-    Enemy* enemy;
-    Hero* hero;
+    Enemy& enemy;
+    Hero& hero;
+    EnemySpawnerTile& enemySpawnerTile;
 
-    EnemyTest()
-    {
-        auto enemySpawnerTile = new EnemySpawnerTile(300, 300, 48, 1, 1, 60, 1000);
-        hero = new Hero(200, 200);
-        enemy = new Enemy(300, 300, reinterpret_cast<EnemySpawnerTile &>(enemySpawnerTile),
-                          reinterpret_cast<GameCharacter &>(hero));
-
-
-    }
-    virtual ~EnemyTest()
-    {
-        delete enemy;
+    EnemyTest(Enemy &enemy, EnemySpawnerTile &enemySpawnerTile, Hero &hero)
+            : enemy(enemy), enemySpawnerTile(enemySpawnerTile), hero(hero) {
+        new EnemySpawnerTile(300, 300, 48, 1, 1, 60, 1000);
+        new Hero(200, 200);
+        new Enemy(300, 300, enemySpawnerTile, hero);
     }
 };
 
 TEST_F(EnemyTest, EnemyAttacksHero){
-    EXPECT_LT(vectorDistance(enemy->getPosition(), hero->getPosition()), 200.f);
+    EXPECT_LT(vectorDistance(enemy.getPosition(), hero.getPosition()), 200.f);
 }
 
